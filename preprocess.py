788dfb9280c708d72fda0f9c3fc9155b78f3d793
Naafiyan Ahmed <naafiyanahmed@Naafiyans-MacBook-Pro.local>
2021-11-20 18:22:24 -0500

    added venv script and gitignore

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,10 @@
+import json
 
+def main():
+    with open('../data/reviews_Baby_5.json', 'r') as f:
+        data = json.load(f)
+    print(data)
+    # with open('../data/processed/data.json', 'w') as f:
+    #     json.dump(data, f)
+if __name__ == '__main__':
+    main()
