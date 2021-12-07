7cb2f611cb2bdd92bc6305c70068ba1b1d0a1267
Naafiyan Ahmed <naafiyanahmed@naafiyans-mbp.devices.brown.edu>
2021-12-07 12:48:41 -0500

    updated to use sequential

--- code/model.py
+++ code/model.py
@@ -3,12 +3,16 @@ import tensorflow as tf
 class Model(tf.keras.Model):
     def __init__ (self, max_length, max_features, batch_size, lr):
         super(Model, self).__init__()
-        self.embedding = tf.keras.layers.Embedding(max_features, 128)
-        self.lstm_1 = tf.keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.2, return_sequences=True)
-        self.lstm_2 = tf.keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.2)
-        self.dense_1 = tf.keras.layers.Dense(64, activation='relu')
-        self.dense_2 = tf.keras.layers.Dense(1, activation='sigmoid')
-        
+      
+        # using sequential
+        self.layers = tf.keras.layers.Sequential([
+            tf.keras.layers.Embedding(max_features, 128, batch_input_shape=[batch_size, max_length]),
+            tf.keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.2, return_sequences=True),
+            tf.keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.2),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
+
         # optimizer
         self.optimizer = tf.keras.optimizers.Adam(lr)
 
@@ -17,18 +21,7 @@ class Model(tf.keras.Model):
         self.batch_size = batch_size
 
     def call(self, texts):
-        # embedding
-        embeddings = self.embedding(texts)
-
-        # lstm
-        lstm_out = self.lstm_1(embeddings)
-        lstm_out = self.lstm_2(lstm_out)
-
-        # dense
-        dense_out = self.dense_1(lstm_out)
-        dense_out = self.dense_2(dense_out)
-
-        return dense_out
+        return self.layers(texts)
     
     def loss(self, labels, predictions):
         # might want to do sparse softmax cross entropy to get 3 types of sentiment
